f20dc9c65bc915cbc81996969b8fc0b0d96900cd
Steven Chorkawy <schorkawy@clarington.net>
2022-08-09 11:30:27 -0400

    first commit.

---
+++ src/webparts/soleSourceRequestForm/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ISoleSourceRequestFormWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'SoleSourceRequestFormWebPartStrings' {
+  const strings: ISoleSourceRequestFormWebPartStrings;
+  export = strings;
+}
