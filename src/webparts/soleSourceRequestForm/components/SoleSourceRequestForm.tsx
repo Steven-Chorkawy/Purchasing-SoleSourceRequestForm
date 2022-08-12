de1da6a73ce6f9efbe3d8166b832054b19806dff
Steven Chorkawy <schorkawy@clarington.net>
2022-08-12 10:19:10 -0400

    Kinda nice.

--- src/webparts/soleSourceRequestForm/components/SoleSourceRequestForm.tsx
+++ src/webparts/soleSourceRequestForm/components/SoleSourceRequestForm.tsx
@@ -34,6 +34,20 @@ export default class SoleSourceRequestForm extends React.Component<ISoleSourceRe
 
     return (
       <div>
+        <div className='row'>
+          <div className='col-lg-1'>1</div>
+          <div className='col-lg-1'>2</div>
+          <div className='col-lg-1'>3</div>
+          <div className='col-lg-1'>4</div>
+          <div className='col-lg-1'>5</div>
+          <div className='col-lg-1'>6</div>
+          <div className='col-lg-1'>7</div>
+          <div className='col-lg-1'>8</div>
+          <div className='col-lg-1'>9</div>
+          <div className='col-lg-1'>10</div>
+          <div className='col-lg-1'>11</div>
+          <div className='col-lg-1'>12</div>
+        </div>
         <h1>Sole Source Request Form</h1>
         <MessageBar messageBarType={MessageBarType.warning}>
           <b>* Client must ensure that the form is reviewed by the Purchasing Division prior to receiving any departmental approval.</b>
@@ -44,9 +58,29 @@ export default class SoleSourceRequestForm extends React.Component<ISoleSourceRe
         <Form
           onSubmit={this.handleSubmit}
           render={(formRenderProps: FormRenderProps) => (
-            <FormElement style={{ maxWidth: 650 }} horizontal={true}>
+            <FormElement horizontal={true}>
               <legend className={"k-form-legend"}>Department Information</legend>
-              
+
+              <div>
+                <div className="row">
+                  <div className="col-lg-6">
+                    lg 6 1
+                  </div>
+                  <div className="col-lg-6">
+                    lg 6 2
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div className="col-sm-6">
+                    sm 6 1
+                  </div>
+                  <div className="col-sm-6">
+                    sm 6 2
+                  </div>
+                </div>
+              </div>
+
               <FieldWrapper>
                 <Label editorId={'Department'} >
                   {'Department'}
